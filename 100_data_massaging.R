873d61207e98330680500cee4ba162b96b78ef5c
Marco Plebani <marcoplebani85@users.noreply.github.com>
2026-04-02 23:42:33 +0200

    Update 100_data_massaging.R

--- 100_data_massaging.R
+++ 100_data_massaging.R
@@ -6,32 +6,18 @@ dd <- data.frame(Lettera = c("B","B","A","A","C","C"),
                  )
 dd
 
-
-
 dd.ordered <- with(dd, dd[order(Lettera), ])
 dd.ordered
 
-
-
 dd$x1
-
-
 
 dd[c(1,2,4),]
 
-
-
 dd[,c(1,3)]
-
-
 
 dd[c(1,2,4), c(1,3)]
 
-
-
 dd[c(1,2,4), c("Lettera", "x1")]
-
-
 
 dd1 <- subset(dd,
               subset=Lettera %in% c("A", "B"),
@@ -39,14 +25,10 @@ dd1 <- subset(dd,
               )
 dd1
 
-
-
 subset(dd,
        subset=Lettera %in% c("A", "B") & x2 < 1,
        select=-c(x1)
        )
-
-
 
 ee <- data.frame(Lettera = c("D","D"),
                  x1 = runif(n=2, min=0, max=1), # valori casuali tra 0 e 1
@@ -54,22 +36,14 @@ ee <- data.frame(Lettera = c("D","D"),
                  )
 ee
 
-
-
 dd2 <- rbind(dd, ee)
 dd2
-
-
 
 x3 <- runif(n=2, min=0, max=1)
 x3
 
-
-
 ee2 <- cbind(ee, x3)
 ee2
-
-
 
 gg <- data.frame(Lettera = c("A", "A", "B","B"),
                  Repliche = c("rep1", "rep2", "rep1","rep2"),
@@ -82,24 +56,16 @@ ff <- data.frame(Lettera = c("B","B", "A", "A"),
                  )
 ff
 
-
-
 mm <- merge(gg, ff, by=c("Lettera", "Repliche"), all=T)
 mm
-
-
 
 vv <- data.frame(Lettera = c("A","B","C","D"),
                  Tipo = c("vocale","consonante","consonante","consonante")
                  )
 vv
 
-
-
 hh <- merge(ff, vv, by=c("Lettera"), all=T)
 hh
-
-
 
 library(pacman)
 p_load(tidyr)
@@ -109,8 +75,6 @@ gg_large <- pivot_wider(data = gg,
                         names_from = "Repliche", 
                         values_from = "x1")
 gg_large
-
-
 
 # cambiamo il formato di gg da lungo a largo:
 gg_long <- pivot_longer(data = gg_large, 
